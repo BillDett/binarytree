e7f8b96454bb79dff451534be7250252ad0456bb
Bill Dettelback <bdettelb@redhat.com>
2022-10-08 20:58:30 -0400

    Changed to binary tree insertion

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,73 +2,91 @@ import java.util.Random;
 
 public class BinaryTree {
 
-    private class Node {
+    public class Node {
         public String id;
         public Node left;
         public Node right;
+
+        public Node() {
+            id = generateId(25);
+            left = right = null;
+        }
     }
 
     private Node root;
     private long elapsed;
+    private long count;
+    private Random rnd = new Random(System.currentTimeMillis());
     private static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
-    public void create(int levels) {
-        System.out.printf("Start Building Tree with %d levels\n", levels);
+    public void insertNodes(int nodes) {
+        System.out.printf("Start Building Tree with %d Nodes\n", nodes);
         startTimer();
-        root = buildTree(levels);
+        for (int i=0; i < nodes; i++) {
+            Node n = new Node();
+            root = insert(root, n);
+        }
         long duration = stopTimer();
-        System.out.printf("Done Building Tree in %f seconds.\n", duration/1000.0);
+        System.out.printf("Done Building Tree in %f seconds.\n", duration/1000.0);  
     }
 
-    public void walk() {
-        System.out.println("Start Walking Tree");
-        startTimer();
-        visit(root);
-        long duration = stopTimer();
-        System.out.printf("Done Walking Tree in %f seconds.\n", duration/1000.0);        
-    }
-    
-    private Node buildTree(int level) {
-        if ( level == 0 ) {
-            return null;
+    private Node insert(Node root, Node n) {
+        if ( root == null ) {
+            root = n;
+            return root;
         } else {
-            Node n = new Node();
-            n.id = generateId(25);
-            n.left = buildTree(level-1);
-            n.right = buildTree(level-1);
-            return n;
+            if (n.id.compareTo(root.id) > 0) {
+                root.right = insert(root.right, n);
+            } else {
+                root.left = insert(root.left, n);
+            }
+            return root;
         }
     }
 
-    private void visit(Node n) {
+    public void walk(boolean show) {
+        count = 0;
+        System.out.println("Start Walking Tree");
+        startTimer();
+        visit(root, show);
+        long duration = stopTimer();
+        System.out.printf("Done Walking Tree, saw %d Nodes in %f seconds.\n", count, duration/1000.0);        
+    }
+    
+    private void visit(Node n, boolean show) {
         if ( n != null ) {
-            n.id.toLowerCase();
-            visit(n.left);
-            visit(n.right);
+            visit(n.left, show);
+            count++;
+            if (show) {
+                System.out.println(n.id);
+            }
+            visit(n.right, show);
         }
     }
 
     private String generateId(int length) {
         String result = new String();
-        Random rnd = new Random(System.currentTimeMillis());
         for ( int i=0; i < length; i++ ) {
             result += chars.charAt(rnd.nextInt(chars.length()));
         }
         return result;
     }
 
-    private void startTimer() { elapsed = System.currentTimeMillis(); }
-    private long stopTimer() { return System.currentTimeMillis() - elapsed; }
+    public void startTimer() { elapsed = System.currentTimeMillis(); }
+    public long stopTimer() { return System.currentTimeMillis() - elapsed; }
 
     public static void  main(String[] args) {
-        int levels = 24;
-        System.out.println(args.length);
+        int nodes = 1000000;
+        boolean showNames = false;
         if ( args.length > 0 ) {
-            levels = Integer.parseInt(args[0]);
+            nodes = Integer.parseInt(args[0]);  // Number of Nodes
+        }
+        if ( args.length > 1 ) {                // Whether to show Node names while walking
+            showNames = true;
         }
         BinaryTree bt = new BinaryTree();
-        bt.create(levels);
-        bt.walk();
+        bt.insertNodes(nodes);
+        bt.walk(showNames);
     }
 
 }
